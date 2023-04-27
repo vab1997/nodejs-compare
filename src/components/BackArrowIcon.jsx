cbfa4ca1e66320295b7a553173de8dbe195f81e4
vab1997 <victorbejas97@gmail.com>
2023-04-27 17:55:17 -0300

    create icons

---
+++ src/components/BackArrowIcon.jsx
@@ -0,0 +1,17 @@
+export default function BackArrowIcon (props) {
+  return (
+    <svg
+      viewBox='0 0 48 48'
+      fill='none'
+      {...props}
+    >
+      <g id='SVGRepo_bgCarrier' stroke-width='0' />
+      <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round' />
+      <g id='SVGRepo_iconCarrier'>
+        <rect width='48' height='48' fill='white' fill-opacity='0.01' />
+        <path d='M5.79891 24L41.7989 24' stroke='#ffffff' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' />
+        <path d='M17.7988 36L5.79883 24L17.7988 12' stroke='#ffffff' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' />
+      </g>
+    </svg>
+  )
+}
---
+++ src/components/CalendarIcon.jsx
@@ -0,0 +1,7 @@
+export default function CalendarIcon (props) {
+  return (
+    <svg viewBox='0 0 24 24' {...props}>
+      <path d='M6 22h12a3 3 0 0 0 3-3V7a2 2 0 0 0-2-2h-2V3a1 1 0 0 0-2 0v2H9V3a1 1 0 0 0-2 0v2H5a2 2 0 0 0-2 2v12a3 3 0 0 0 3 3Zm-1-9.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5V19a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1Z' />
+    </svg>
+  )
+}
