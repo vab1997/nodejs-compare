a6d10d487acc6ac280f7b7e693b68cb5ede21f5b
vab1997 <victorbejas97@gmail.com>
2023-04-30 21:59:37 -0300

    change folder name

--- src/app/charts/[dates]/page.jsx
+++ src/app/charts/[dates]/page.jsx
@@ -2,28 +2,27 @@ import BackArrowIcon from '@/components/BackArrowIcon'
 import BadgeComponent from '@/components/Badge'
 import DonutChartComponent from '@/components/DonutChart'
 import LineChartComponent from '@/components/LineChart'
-import { Stars } from '@/components/Star'
+import Stars from '@/components/Star'
 import useGetDataChart from '@/hooks/useGetDataChart'
 
 export default async function PageLineChart ({ params }) {
   const { dates } = params
-
   const { totalDownload, dataLineChart, dataDonutChart } = await useGetDataChart({ dates })
 
   return (
-    <main className='flex flex-col items-center justify-center py-12 w-full'>
+    <main className='flex flex-col items-center justify-center py-8 md:py-12 w-full'>
       <div className='flex flex-col items-center justify-center max-w-6xl w-full'>
-        <header className='flex justify-between gap-8 w-full py-6'>
-          <a href='/' className='flex gap-2 items-center text-lg font-bold text-white hover:underline hover:opacity-80'>
+        <header className='flex justify-between gap-8 w-full px-2 py-6 mb-4'>
+          <a href='/' className='flex gap-2 items-center text-md font-bold text-white hover:underline md:text-lg hover:opacity-80'>
             <BackArrowIcon width={24} height={24} />
-            Home
+            Back to home
           </a>
           <Stars />
         </header>
 
         <section className='flex flex-col items-center justify-center gap-16 w-full'>
-          <div className='flex items-center gap-2'>
-            <h1 className='text-white font-bold text-xl'>Total download per date of all version:</h1>
+          <div className='flex flex-col justify-content items-center gap-2 md:flex-row'>
+            <h1 className='text-white font-bold text-lg md:text-xl'>Total downloads by date of all versions:</h1>
             <BadgeComponent dates={dates} totalDownload={totalDownload} />
           </div>
           <LineChartComponent dataLineChart={dataLineChart} />
