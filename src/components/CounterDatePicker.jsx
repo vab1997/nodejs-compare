bf3a3613f7f4b12a33e04fa8dc9ef80e2039896a
vab1997 <victorbejas97@gmail.com>
2023-04-30 22:05:16 -0300

    add validation

--- src/components/CounterDatePicker.jsx
+++ src/components/CounterDatePicker.jsx
@@ -23,27 +23,35 @@ export default function CounterDatePicker () {
 
   return (
     <div className='flex flex-col items-center justify-center gap-4'>
-      <div className='flex items-center justify-center gap-2'>
-        <h3 className='text-white'>How many dates do you want to compare?</h3>
-        <select className='' onChange={(e) => setCountDate(e.target.value)}>
-          <option value='1'>one date</option>
-          <option value='2'>two dates</option>
-          <option value='3'>three dates</option>
+      <div className='flex flex-col items-center justify-center gap-2 md:flex-row'>
+        <h3 className='text-white text-lg'>How many dates do you want to compare?</h3>
+        <select
+          className='bg-transparent outline-none focus:border-[#8CC84B] border border-white/10 hover:border-[#8CC84B] rounded-full text-white md:text-lg px-2 py-[6px] md:py-[9px]'
+          onChange={(e) => setCountDate(e.target.value)}
+        >
+          <option selected disabled className='text-white bg-[#13111C]' value='0'>Select</option>
+          <option className='text-white bg-[#13111C]' value='1'>One date</option>
+          <option className='text-white bg-[#13111C]' value='2'>Two dates</option>
+          <option className='text-white bg-[#13111C]' value='3'>Three dates</option>
         </select>
       </div>
 
-      <div className='flex items-center justify-center gap-4'>
+      <div className='flex flex-col items-center justify-center gap-4 mt-4 md:flex-row'>
         {arrayCountDate.map(key => (
           <CalendarPicker key={key} label={key} handleDates={handleDates} />
         ))}
       </div>
 
-      <Link
-        className='text-white'
-        href={`/lineChart/${dates.flatMap(date => Object.values(date)).filter(date => date !== '').join('-')}`}
-      >
-        Compare
-      </Link>
+      {(dates[0][1] !== '') || (dates[1][2] !== '') || (dates[2][3] !== '')
+        ? (
+          <Link
+            className='bg-transparent outline-none focus:border-[#8CC84B] border border-white/10 hover:border-[#8CC84B] rounded-full text-white text-center md:text-lg px-6 py-[6px] md:py-[9px]'
+            href={`/charts/${dates.flatMap(date => Object.values(date)).filter(date => date !== '').join('-')}`}
+          >
+            Compare
+          </Link>
+          )
+        : null}
     </div>
   )
 }
