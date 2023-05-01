46dad891143cf24d6068e89793965d4ed89f57e5
vab1997 <victorbejas97@gmail.com>
2023-04-30 22:02:18 -0300

    modify layout main page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,7 @@
 import './globals.css'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import InfoUpdate from '@/components/InfoUpdate'
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,7 +11,12 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <Header />
+        <InfoUpdate />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,7 @@ import CounterDatePicker from '@/components/CounterDatePicker'
 export default function Home () {
   return (
     <main className='flex items-center justify-center py-16 px-12'>
-      <div className='flex flex-col items-center justify-center gap-8 max-w-6xl w-full'>
-        <h1 className='text-[#8CC84B] font-medium text-5xl'>Nodejs Compare</h1>
-
-        <CounterDatePicker />
-      </div>
+      <CounterDatePicker />
     </main>
   )
 }
