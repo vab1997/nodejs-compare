bb11dec69e9326afea3113f360bd1276a6825256
vab1997 <victorbejas97@gmail.com>
2023-04-30 22:04:55 -0300

    create component header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import NodejsIcon from '@/components/NodejsIcon'
+
+export default function Header () {
+  return (
+    <header className='flex items-center justify-center flex-col gap-6 pt-8 pb-4 lg:pb-6 lg:pt-12'>
+      <h1 className='gap-2 text-center text-5xl font-bold text-[#8CC84B] md:text-6xl lg:text-8xl md:flex md:justify-center md:items-center'>
+        Downloads Node.js
+        <span className='flex items-center justify-center pt-2'>
+          <NodejsIcon />
+        </span>
+      </h1>
+      <h2 className='text-lg font-medium text-center text-white lg:text-4xl md:text-2xl'>
+        Compare Nodejs download counts over time
+      </h2>
+    </header>
+  )
+}
