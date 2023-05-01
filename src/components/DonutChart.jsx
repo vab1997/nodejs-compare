734cb9a3ad2c89a970f76f5a3098e51681da33b3
vab1997 <victorbejas97@gmail.com>
2023-04-30 22:05:01 -0300

    change styles

--- src/components/DonutChart.jsx
+++ src/components/DonutChart.jsx
@@ -5,9 +5,9 @@ import CalendarIcon from './CalendarIcon'
 
 export default function DonutChartComponent ({ dataDonutChart }) {
   return (
-    <div className='flex flex-col justify-center gap-6 w-full'>
+    <div className='flex flex-col justify-center gap-6 px-2 w-full'>
       <h2 className='text-white font-medium text-lg'>Downloads count in the days in the countrys:</h2>
-      <div className='flex items-center justify-center gap-2 w-full'>
+      <div className='flex flex-col items-center justify-center gap-2 w-full md:flex-row'>
         {dataDonutChart.map(({ date, dataCountry }) => (
           <Card key={date} className='bg-transparent p-4 max-w-sm w-full ring-0 ring-tranparent shadow-none'>
             <Title className='flex items-center justify-center gap-2 text-white text-center'>
